3bac19e53f3cd96c4a2ffc8f6cab5f18a7bbb927
destiny <2425052816@qq.com>
2019-08-31 12:26:29 +0800

    new 还是在重新请求---detail

--- vue-month-exam/vue.config.js
+++ vue-month-exam/vue.config.js
@@ -3,15 +3,16 @@ module.exports = {
         open: true,
         before(app) {
             let data = require('./src/data/data.json')
-            let newData1;
+            let newData = newData1 = data.ranklist1;
             // app.use(bodyParser.urlencoded({ extended: false }))
             // app.use(bodyParser.json())
             app.get('/list_default', (req, res) => {
-                newData1 = data.ranklist1;
+
                 res.send({ code: 200, values: newData1 })
             })
             app.get('/get_detail', (req, res) => {
                 let { id } = req.query
+                console.log(newData)
                 let detailItem = newData1.filter(item => {
                     if (item.bookId * 1 == id * 1) {
                         return item
